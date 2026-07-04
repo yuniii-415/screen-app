d37ba8e2742b5a09853cd505fc05e7b6a2c59df1
yuniii-415 <seng_yun_hee@students.edu.sg>
2026-07-04 10:15:46 +0800

    Added background image and name

--- ScreenApp/ContentView.swift
+++ ScreenApp/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.blue
+                .ignoresSafeArea()
+            Text("Yun Hee")
+                .font(.largeTitle)
+                .bold()
+                .foregroundStyle(.white)
         }
-        .padding()
     }
 }
 
