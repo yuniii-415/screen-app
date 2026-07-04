2002cab4943d8aa772968b384620590e16053714
yuniii-415 <seng_yun_hee@students.edu.sg>
2026-07-04 10:05:28 +0800

    Initial Commit

---
+++ ScreenApp/ScreenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenAppApp.swift
+//  ScreenApp
+//
+//  Created by T Krobot on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
